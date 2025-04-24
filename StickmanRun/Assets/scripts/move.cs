e3dbfc6a568a61e05c701192bf422af6a25bed48
DanyDiab <danydiab04@gmail.com>
2025-04-24 16:12:13 -0300

    added gravity, collision with floor, and new sprite for the player

--- StickmanRun/Assets/scripts/move.cs
+++ StickmanRun/Assets/scripts/move.cs
@@ -5,31 +5,62 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     private int speed;
+    private float gconstant;
+    private float gforce;
+    private bool jumped; 
+    private bool onFloor;
+
     // Start is called before the first frame update
     void Start()
     {
-        speed = 5;
+        speed = 2;
+        gconstant = .04f;
+        gforce = 0;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate(){
+        applyGravity();
         // move to the right by default
-        float AdjustedSpeed = speed * Time.deltaTime;
-        Vector3 newPos = new Vector3(transform.position.x + AdjustedSpeed,transform.position.y,1); 
-        transform.position = newPos;
-
-
-        // check if the player is jumping
-        if(Input.GetKeyDown(KeyCode.Space)){
-
+        if(Input.GetKey(KeyCode.Space) && onFloor){
+            jump();
         }
-
-
-        // check if the player is on the ground
-
-        // if()
+        float AdjustedSpeed = speed * Time.deltaTime;
+        Vector3 newPos = new Vector3(transform.position.x + AdjustedSpeed,transform.position.y - gforce,1); 
+        transform.position = newPos;
     }
 
-     
+
+    void applyGravity(){
+        if(onFloor){
+            gforce = 0;
+            return;
+        }
+        else if(gforce < 2){
+            // add an initial gravity force
+            if(gforce == 0){
+                gforce = .04f;
+            }
+            // continue adding to the gravity force
+            gforce += gconstant * Time.deltaTime;
+        }
+    }
+    void jump(){
+        gforce -= .04f;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Floor")){
+            onFloor = true;
+            return;
+        }
+    }
+
+        void OnCollisionExit2D(Collision2D collision){
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Floor")){
+            onFloor = false;
+            return;
+        }
+    }
+
+
 }
