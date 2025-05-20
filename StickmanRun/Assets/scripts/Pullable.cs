f81819c8d40761ee909a59a72635dee5769dc550
DanyDiab <danydiab04@gmail.com>
2025-05-20 14:38:45 -0300

    added tree sprite, added light to chandeliers, crate, and player

--- StickmanRun/Assets/scripts/Pullable.cs
+++ StickmanRun/Assets/scripts/Pullable.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class Pullable : MonoBehaviour
 {
     bool isPulled;
+    public bool isStuck;
     Rigidbody2D rb;
+    float gScale;
     // Start is called before the first frame update
 
 
     void Start()
     {
+        gScale = 2;
         rb = GetComponent<Rigidbody2D>();
     }
+    void LateUpdate()
+    {
+        if (isPulled && isStuck)
+        {
+            isStuck = false;
+            rb.gravityScale = gScale;
+            return;
+        }
+        if (isStuck)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = Vector2.zero;
+        } 
+    }
 
-    public void setIsPulled(bool pulled){
+    public void setIsPulled(bool pulled)
+    {
         isPulled = pulled;
     }
 
