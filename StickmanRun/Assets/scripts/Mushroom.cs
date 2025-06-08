a76886cc06da3bdea3457d1d1bb400038ca5564b
DanyDiab <danydiab04@gmail.com>
2025-06-08 11:57:10 -0300

    fixed bug with catapult rock going through walls, fixed mushroom double jumping bug

--- StickmanRun/Assets/scripts/Mushroom.cs
+++ StickmanRun/Assets/scripts/Mushroom.cs
@@ -8,6 +8,8 @@ public class Mushroom : MonoBehaviour
 {
     float jumpBoost;
     float minBoost;
+    float jumpCooldown;
+    float lastJumpTime;
     Rigidbody2D jumpedRb;
     AirTime airTime;
     bool addForces;
@@ -18,11 +20,13 @@ public class Mushroom : MonoBehaviour
     bool jumped;
     SoundManager soundManager;
     public AudioClip jumpSound;
+    String jumpedTag;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        jumpCooldown = .1f;
         objectShake = GetComponent<ObjectShake>();
         minBoost = 70f;
         maxForce = 25f;
@@ -36,68 +40,33 @@ public class Mushroom : MonoBehaviour
     {
         if (other.CompareTag("Grappler")) return;
         jumpedRb = other.attachedRigidbody;
-        if (jumpedRb != null && !jumped)
+        if (jumpedRb != null && Time.time - lastJumpTime > jumpCooldown)
         {
-            soundManager.playSound(jumpSound);
-            jumped = true;
-            objectShake.objectShake();
+            jumpedTag = other.tag;
+            Debug.Log("jumped");
             jumpBoost = calculateJumpBoost();
-            Debug.Log(transform.up);
-            float jumpX = transform.up.x;
-            float currX = jumpedRb.velocity.x;
-            if((jumpX > 0 && currX < 0) || (jumpX < 0 && currX > 0)) jumpedRb.velocity = new Vector2(jumpedRb.velocity.y,-jumpedRb.velocity.x);
+            reverseDirIfNeeded();
+            lastJumpTime = Time.time;
+
             jumpedRb.AddForce(transform.up * jumpBoost, ForceMode2D.Impulse);
-            maxAirTime = 1f;
-            addForces = true;
-
+            soundManager.playSound(jumpSound);
+            objectShake.objectShake();
         }
     }
 
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (jumped)
-        {
-            jumped = !jumped;
-        }
-    }
-
-    // Vector2 calculateDirToJump()
-    // {
-    //     Vector2 normal = transform.up;
-    // }
     float calculateJumpBoost()
     {
         float velocityBounce = Math.Abs(jumpedRb.velocity.x) * 3.5f;
         return Mathf.Max(velocityBounce, minBoost);
     }
 
-    void FixedUpdate()
-    {
-        if (addForces)
-        {
-            jump();
-            return;
-        }
-    }
 
-    void jump()
-    {
-        // timeInAir += Time.deltaTime;
-        // if y velocity is negative make it positive
-        //
-        // float t = timeInAir / maxAirTime;
-        // float forceScale = Mathf.Clamp01(1 - t);
-        // float v = jumpBoost * forceScale * jumpedRb.gravityScale;
-        // if (maxAirTime < .1f)
-        // {
-        // v = jumpBoost * jumpedRb.gravityScale;
-        // }
-        // if (v > maxForce) v = maxForce;
-        // v += currYVelocity;
-        
-        // if (timeInAir > maxAirTime)
-        // {
-        //     addForces = false;
-        // }
+/// <summary>
+/// Reverses the direction that the jumpedRb is going in if the bounce send him in an opposite direction
+/// </summary>
+    void reverseDirIfNeeded(){
+        float jumpX = transform.up.x;
+        float currX = jumpedRb.velocity.x;
+        if((jumpX > 0 && currX < 0) || (jumpX < 0 && currX > 0)) jumpedRb.velocity = new Vector2(jumpedRb.velocity.y,-jumpedRb.velocity.x);
     }
 }
